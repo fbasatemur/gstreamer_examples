d779926f8969441787dd08b03d300aa8809ffe93
froot <froot@pop-os.localdomain>
2022-12-28 23:18:34 +0300

    pipeline was updated

--- config.hpp
+++ config.hpp
@@ -13,13 +13,18 @@ namespace GST_PARAMS{
     // webcam_server_pipeline: gst-launch-1.0 -v v4l2src device=/dev/video0 ! image/jpeg,width=1280,height=720,type=video,framerate=30/1 ! jpegdec ! videoscale ! videoconvert ! x264enc tune=zerolatency ! rtph264pay ! udpsink host=127.0.0.1 port=5000 --verbose
     // webcam_client_pipeline: gst-launch-1.0 -vc udpsrc port=5000 close-socket=false multicast-iface=false auto-multicast=true ! application/x-rtp, payload=96 ! rtpjitterbuffer ! rtph264depay ! avdec_h264 ! fpsdisplaysink  sync=false --verbose
 
-    static std::string webcam_client_pipeline = std::string{"gst-launch-1.0 -vc udpsrc port=" + multicast_port + 
-                                                            " close-socket=false multicast-iface=false auto-multicast=true ! application/x-rtp, payload=96 "
-                                                            "! rtpjitterbuffer ! rtph264depay ! avdec_h264 ! fpsdisplaysink  sync=false --verbose"};
+//    static std::string webcam_client_pipeline = std::string{"udpsrc multicast-group=" + multicast_ip + " port=" + multicast_port +
+//                                                             " ! application/x-rtp, media=video, encoding-name=H264 ! rtpjitterbuffer ! rtph264depay ! h264parse ! avdec_h264 ! xvimagesink name=appsink0"};
+
+    static std::string webcam_client_pipeline = std::string{"udpsrc multicast_group=127.0.0.1 port=5000 auto-multicast=true ! application/x-rtp, payload=96 ! rtpjitterbuffer ! rtph264depay ! avdec_h264 ! appsink name=appsink0"};
+
+
+
+
 
     static const std::string video_path{"../video/g_video.mp4"};
 
-    static std::string filesrc_server_pipeline = std::string{"gst-launch-1.0 filesrc location=" + video_path + 
+    static std::string filesrc_server_pipeline = std::string{"filesrc location=" + video_path +
                                                                 " ! qtdemux name=d d. ! h264parse ! rtph264pay config-interval=3 pt=96 mtu=1024"
                                                                 " ! udpsink host=" + multicast_ip + " port=" + multicast_port};
 
--- gst_multicast_handler.hpp
+++ gst_multicast_handler.hpp
@@ -18,15 +18,13 @@
 struct GST_Struct{
     
     GstElement *appsink, *gst_pipeline;
-
     GstSample* sample;
     GstBuffer* buffer;
     GstMapInfo map;
 
     cv::Size frame_shape;     
     cv::Mat frame;
-
-
+    bool is_initialized{false};
     GST_Struct(): appsink(nullptr), gst_pipeline(nullptr), sample{nullptr}, buffer{nullptr}{ gst_init(nullptr, nullptr); }
 
     void SetState(const char* pipeline){
@@ -37,16 +35,15 @@ struct GST_Struct{
     }
     
     static GstFlowReturn Callback(GstElement* appsink, GST_Struct* gst_self){
-        
         g_signal_emit_by_name (appsink, "pull-sample", &gst_self->sample, NULL);
 
         gst_self->buffer = gst_sample_get_buffer (gst_self->sample);
         GST_CHECK(gst_self->buffer, "get buffer is null");
 
+
         gst_buffer_map (gst_self->buffer, &gst_self->map, GST_MAP_READ);
         
-        if(gst_self->frame.empty()){
-
+        if(not gst_self->is_initialized){
             const GstStructure* info = NULL;
             GstCaps* caps = NULL;
 
@@ -59,16 +56,20 @@ struct GST_Struct{
             // convert gstreamer data to OpenCV Mat
             gst_structure_get_int (info, "width", &gst_self->frame_shape.width);
             gst_structure_get_int (info, "height", &gst_self->frame_shape.height);
-
-            gst_self->frame = cv::Mat(gst_self->frame_shape, CV_8UC3, (unsigned char*)gst_self->map.data, cv::Mat::AUTO_STEP);
+            gst_self->is_initialized = true;
         }
-        
+
+        gst_self->frame = cv::Mat(gst_self->frame_shape, CV_8UC3, (unsigned char*)gst_self->map.data, cv::Mat::AUTO_STEP);
         return GST_FLOW_OK;
     }
 
     void TryAppsink(char* appsink_str, int sink_no = 0){
         try{
-            appsink = gst_bin_get_by_name(GST_BIN(gst_pipeline), (std::string(appsink_str) + std::to_string(sink_no)).c_str());
+            appsink = gst_bin_get_by_name(GST_BIN(gst_pipeline), (const gchar*)(std::string(appsink_str) + std::to_string(sink_no)).c_str());
+            if (not appsink){
+                std::cerr << "appsink is NULL !\n";
+                exit(0);
+            }
         }catch(std::exception &e)
         {
             std::cerr << e.what() << std::endl;
@@ -76,16 +77,22 @@ struct GST_Struct{
         }
     }
 
-    cv::Mat&& GetFrameFromConnection(){
-
+    void StartCallBack(){
+        g_object_set (appsink, "emit-signals", TRUE, NULL);
         g_signal_connect(appsink, "new-sample", G_CALLBACK(Callback), this);
+    }
+
+    cv::Mat GetFrameFromConnection(){
+        while (frame.empty()){
+            std::cerr << "Frame is not exist !\n";
+        }
         return frame.clone();
     }
 
     ~GST_Struct(){
         gst_element_set_state (gst_pipeline, GST_STATE_NULL);
-        gst_object_unref (gst_pipeline);
-        gst_object_unref (appsink);
+        if(gst_pipeline) gst_object_unref (gst_pipeline);
+        if(appsink) gst_object_unref (appsink);
         if(buffer) gst_buffer_unmap (buffer, &map);
         if(sample) gst_sample_unref (sample);
     }
@@ -95,13 +102,14 @@ struct GST_Struct{
 class MulticastUDPHandler{
 
     friend struct GST_Struct;
-    static GST_Struct gst_stream;
+    inline static GST_Struct gst_stream;
 public:
     MulticastUDPHandler() = default;
 
     static void StartListen(const char* pipeline){
         gst_stream.SetState(pipeline);
         gst_stream.TryAppsink("appsink");
+        gst_stream.StartCallBack();
     }
 
     static cv::Mat&& GetFrame(){
